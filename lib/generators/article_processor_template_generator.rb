d055b218dee4db8cacb014a35d42745e19fe8c00
ryotaro uchigane <ryotaro.uchigane@theport.jp>
2018-02-15 17:12:17 +0900

    rename template dir name

--- lib/generators/article_processor_template_generator.rb
+++ lib/generators/article_processor_template_generator.rb
@@ -3,6 +3,6 @@ class ArticleProcessorTemplateGenerator < Rails::Generators::Base
 
   def create_template
     directory('./article_processor/', 'app/processors/')
-    create_file('./article_processor_helper/processor_helper.rb', 'app/helpers/processor_helper.rb')
+    copy_file('./article_processor_helper/processor_helper.rb', 'app/helpers/processor_helper.rb')
   end
 end
